1fc456beb61360c0be1719d7509d0505a3edbd9d
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-15 17:13:27 +0200

    Add `GitHubExplorerApplication`

---
+++ app/src/main/java/io/github/frankolt/githubexplorer/ui/GitHubExplorerApplication.kt
@@ -0,0 +1,7 @@
+package io.github.frankolt.githubexplorer.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GitHubExplorerApplication : Application()
