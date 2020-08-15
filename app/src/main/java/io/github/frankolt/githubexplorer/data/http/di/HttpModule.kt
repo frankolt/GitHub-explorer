527c33927e1523cc0b2678af724aa44045e286c8
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-15 22:29:29 +0200

    Add `HttpModule`

---
+++ app/src/main/java/io/github/frankolt/githubexplorer/data/http/di/HttpModule.kt
@@ -0,0 +1,24 @@
+package io.github.frankolt.githubexplorer.data.http.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object HttpModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .build()
+}
