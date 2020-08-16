8791c5271625d9f726e47b683feb987d94ba4133
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-16 13:45:06 +0200

    Implement query `LiveData`

--- app/src/main/java/io/github/frankolt/githubexplorer/ui/search/SearchFragment.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/ui/search/SearchFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.frankolt.githubexplorer.databinding.FragmentSearchBinding
+import io.github.frankolt.githubexplorer.ui.extensions.update
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
@@ -30,6 +33,20 @@ class SearchFragment : Fragment() {
     ): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observe()
+        setupUi()
+    }
+
+    private fun observe() {
+        viewModel.query.observe(viewLifecycleOwner) { binding.queryInputField.update(it) }
+    }
+
+    private fun setupUi() {
+        binding.queryInputField.addTextChangedListener { viewModel.search(it.toString()) }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/io/github/frankolt/githubexplorer/ui/search/SearchViewModel.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/ui/search/SearchViewModel.kt
@@ -1,8 +1,21 @@
 package io.github.frankolt.githubexplorer.ui.search
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class SearchViewModel @ViewModelInject constructor(
 ) : ViewModel() {
+
+    private var _query = MutableLiveData<String>()
+
+    val query: LiveData<String>
+        get() = _query
+
+    fun search(query: String) = viewModelScope.launch {
+        _query.value = query
+    }
 }
