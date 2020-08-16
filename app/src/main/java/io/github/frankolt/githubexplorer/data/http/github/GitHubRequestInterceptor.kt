3ab194c91bda021dd4e9c0645198c6e84410bd48
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-16 12:33:11 +0200

    Add the `@Inject` annotation to the constructor of `GitHubRequestInterceptor`

--- app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/GitHubRequestInterceptor.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/GitHubRequestInterceptor.kt
@@ -1,8 +1,10 @@
 package io.github.frankolt.githubexplorer.data.http.github
 
 import okhttp3.Interceptor
+import javax.inject.Inject
 
-class GitHubRequestInterceptor : Interceptor {
+class GitHubRequestInterceptor @Inject constructor(
+) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain) = chain.request().newBuilder()
         .addHeader(GIT_HUB_HEADER_ACCEPT_KEY, GIT_HUB_HEADER_ACCEPT_VALUE)
