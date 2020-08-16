2c935e5223585c77001e383d8d16cb3a2f5c61d1
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-16 13:44:06 +0200

    Plant `DebugTree`

--- app/src/main/java/io/github/frankolt/githubexplorer/ui/GitHubExplorerApplication.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/ui/GitHubExplorerApplication.kt
@@ -2,6 +2,16 @@ package io.github.frankolt.githubexplorer.ui
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.github.frankolt.githubexplorer.BuildConfig
+import timber.log.Timber
 
 @HiltAndroidApp
-class GitHubExplorerApplication : Application()
+class GitHubExplorerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
