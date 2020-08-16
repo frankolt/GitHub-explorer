39e62d0cf53a22b91a8dab2882b6280269c49c4e
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-16 09:55:40 +0200

    Add the `/search/repositories` endpoint

--- app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/GitHubService.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/GitHubService.kt
@@ -1,4 +1,17 @@
 package io.github.frankolt.githubexplorer.data.http.github
 
+import io.github.frankolt.githubexplorer.data.github.SearchResult
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface GitHubService {
+
+    @GET("/search/repositories")
+    suspend fun search(
+        @Query("q") q: String,
+        @Query("sort") sort: String? = null,
+        @Query("order") order: String? = null,
+        @Query("per_page") perPage: Int? = null,
+        @Query("page") page: Int? = null
+    ): SearchResult
 }
