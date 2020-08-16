53bcc469cf9b417a80f28e91446a24f85256de2f
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-16 12:41:50 +0200

    Add the `update` extension for `EditText`

---
+++ app/src/main/java/io/github/frankolt/githubexplorer/ui/extensions/EditText.kt
@@ -0,0 +1,20 @@
+package io.github.frankolt.githubexplorer.ui.extensions
+
+import android.widget.EditText
+
+/**
+ * Updates the text only if necessary. This is to prevent calling the listeners when setting the
+ * same text. Facilitates the MVVM architecture by not propagating an additional call to the
+ * `ViewModel` when setting the text from the `LiveData`.
+ *
+ * (For example, when a user updates an input field, the `ViewModel` is notified and updates the
+ * appropriate `LiveData` object. This results in updating the input field with the same text, which
+ * results in notifying the `ViewModel` again. The loop stops here, because the `LiveData` will not
+ * update the field again. However, this is undesirable if, for example, an API call is done on a
+ * text update in the `ViewModel`.)
+ */
+fun EditText.update(text: CharSequence) {
+    if (this.text.toString() != text) {
+        this.setText(text)
+    }
+}
