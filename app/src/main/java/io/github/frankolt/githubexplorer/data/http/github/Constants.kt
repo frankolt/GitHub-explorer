ff4565d7352d8e7d26f53a2f90c4323fdd589c64
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-08-23 22:10:16 +0200

    Add user agent header

--- app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/Constants.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/Constants.kt
@@ -2,3 +2,6 @@ package io.github.frankolt.githubexplorer.data.http.github
 
 const val GIT_HUB_HEADER_ACCEPT_KEY = "Accept"
 const val GIT_HUB_HEADER_ACCEPT_VALUE = "application/vnd.github.v3+json"
+
+const val GIT_HUB_HEADER_USER_AGENT_KEY = "User-Agent"
+const val GIT_HUB_HEADER_USER_AGENT_VALUE = "io.github.frankolt.githubexplorer"
--- app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/GitHubRequestInterceptor.kt
+++ app/src/main/java/io/github/frankolt/githubexplorer/data/http/github/GitHubRequestInterceptor.kt
@@ -7,7 +7,8 @@ class GitHubRequestInterceptor @Inject constructor(
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain) = chain.request().newBuilder()
-        .addHeader(GIT_HUB_HEADER_ACCEPT_KEY, GIT_HUB_HEADER_ACCEPT_VALUE)
+        .header(GIT_HUB_HEADER_USER_AGENT_KEY, GIT_HUB_HEADER_USER_AGENT_VALUE)
+        .header(GIT_HUB_HEADER_ACCEPT_KEY, GIT_HUB_HEADER_ACCEPT_VALUE)
         .build()
         .let { chain.proceed(it) }
 }
