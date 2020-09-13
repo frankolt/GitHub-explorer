9b6acc057e5cfc293e4a93457142713666829c7f
Franko Leon Tokalić <franko.tokalic@gmail.com>
2020-09-13 20:16:19 +0200

    Introduce the `InteractorModule` Dagger module

---
+++ app/src/main/java/io/github/frankolt/githubexplorer/domain/github/interactors/di/InteractorModule.kt
@@ -0,0 +1,10 @@
+package io.github.frankolt.githubexplorer.domain.github.interactors.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+interface InteractorModule {
+}
